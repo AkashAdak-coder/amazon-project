b78c1a2213c510c1e42878f9d90e942d2164db1b
AkashAdak-coder <akashadak2608@gmail.com>
2026-03-21 20:13:24 +0530

    add a dolor symbol

--- script/amazon.js
+++ script/amazon.js
@@ -18,7 +18,7 @@ products.forEach( (product) => {
                 <div class="review-count">${product.rating.count}</div>
             </div>
 
-            <div class="product-price">
+            <div class="product-price">$
                 ${((product.priceCents) / 100).toFixed(2)}
             </div>
 
